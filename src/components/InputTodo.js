1a1b2d48efc73a5900040f1087c545ae6c4b3be4
salam <es_belhadj@esi.dz>
2022-02-21 23:09:17 +0100

    add form submition

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,12 +1,25 @@
 /* eslint-disable react/prefer-stateless-function */
 /* eslint-disable react/state-in-constructor */
 /* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 
 import React, { Component } from 'react';
 
 class InputTodo extends Component {
     state = {
       title: '',
+    }
+
+    handleSubmit = (e) => {
+      e.preventDefault();
+      if (this.state.title.trim()) {
+        this.props.addTodoItem(this.state.title);
+        this.setState({
+          title: '',
+        });
+      } else {
+        alert('Please write item');
+      }
     }
 
     onChange = (e) => {
@@ -17,7 +30,7 @@ class InputTodo extends Component {
 
     render() {
       return (
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input
             type="text"
             placeholder="Add Todo..."
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -3,6 +3,7 @@
 /* eslint-disable react/no-access-state-in-setstate */
 
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import TodosList from './TodosList';
 import Header from './Header';
 import InputTodo from './InputTodo';
@@ -11,17 +12,17 @@ class TodoContainer extends React.Component {
     state = {
       todos: [
         {
-          id: 1,
+          id: uuidv4(),
           title: 'Setup development environment',
           completed: true,
         },
         {
-          id: 2,
+          id: uuidv4(),
           title: 'Develop website and add content',
           completed: false,
         },
         {
-          id: 3,
+          id: uuidv4(),
           title: 'Deploy to live server',
           completed: false,
         },
@@ -52,11 +53,22 @@ class TodoContainer extends React.Component {
       });
     };
 
+    addTodoItem = (title) => {
+      const newTodo = {
+        id: uuidv4(),
+        title,
+        completed: false,
+      };
+      this.setState({
+        todos: [...this.state.todos, newTodo],
+      });
+    };
+
     render() {
       return (
         <div>
           <Header />
-          <InputTodo />
+          <InputTodo addTodoItem={this.addTodoItem} />
           <TodosList
             todos={this.state.todos}
             handleChangeCheck={this.handleChange}
