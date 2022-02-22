b1a06b98bf2e6c1d435742ef57d571f508940791
salam <es_belhadj@esi.dz>
2022-02-22 22:15:48 +0100

    add React lifecycle methods.

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -14,9 +14,20 @@ class TodoContainer extends React.Component {
   };
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
-      .then((response) => response.json())
-      .then((data) => this.setState({ todos: data }));
+    const temp = localStorage.getItem('todos');
+    const loadedTodos = JSON.parse(temp);
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos);
+      localStorage.setItem('todos', temp);
+    }
   }
 
     handleChange = (id) => {
